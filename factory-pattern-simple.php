980781725a67760bba0eeb4e58784606705d69ca
xjin <mrtop@live.cn>
2015-03-05 13:43:08 +0800

    工厂模式

--- factory-pattern-simple.php
+++ factory-pattern-simple.php
@@ -1,27 +1,79 @@
 <?php
 
 /**
- * Simple Factory-pattern 简单工程模式
- * 
- * @abstract
+ * Factory-pattern 工厂模式
  *
- *
+ * @abstract 定义一个用于创建对象的接口，让子类决定实例化哪一个类。Factory Method 使一个类的实例化延迟到其子类。
  * @author xjin
  * @version 2015/3/4
  */
 
-class Fruit{
-    // 对象从工厂类返回
+interface IImage {
+    public function getHeight();
 }
 
-class FruitFactory{
-
-    public static function factory() {
-        // 返回对象的一个新实例
-        return new Fruit();
+// Png 图片处理
+class Image_PNG implements IImage {
+    private $_width, $_height, $_data;
+    public function __construct($file)
+    {
+        $this->_file = $file;
+        $this->_parse ();
     }
-
+    private function _parse()
+    {
+        // 完成PNG格式的解析工作
+        // 并填充$_width,$_height,$_data;
+    }
+    public function getHeight()
+    {
+        return $this->_height;
+    }
 }
 
-// 调用工厂
-$instance = FruitFactory::factory();
+// Jpeg 图片处理
+class Image_JPEG implements IImage {
+    private $_width, $_height, $_data;
+    public function __construct($file)
+    {
+        $this->_file = $file;
+        $this->_parse ();
+    }
+    private function _parse()
+    {
+        // 完成JPEG格式的解析工作
+        // 并填充$_width,$_height,$_data;
+    }
+    public function getHeight()
+    {
+        return $this->_height;
+    }
+}
+
+// 工厂模式的应用
+class ImageFactory {
+    public static function factory($file)
+    {
+        $pathParts = pathinfo ( $file );
+        switch (strtolower ( $pathParts ['extension'] ))
+        {
+            case 'jpg' :
+                $ret = new Image_JPEG ( $file );
+                break;
+            case 'png' :
+                $ret = new Image_PNG ( $file );
+                break;
+            default :
+            // 有问题
+        }
+        if ($ret instanceof IImage)
+        {
+            return $ret;
+        } else
+        {
+            // 有问题
+        }
+    }
+}
+   
+//当使用图像文件名调用 工厂方法时，根据传入的文件类型不同，取得不同对象。
--- factory-pattern.php
+++ factory-pattern.php
@@ -1,23 +1,27 @@
 <?php
 
 /**
- * Factory-pattern  工厂模式
- * User: xjin
- * Date: 2015/3/4
- * Time: 22:43
+ * Factory-pattern 工厂模式
+ *
+ * @abstract 定义一个用于创建对象的接口，让子类决定实例化哪一个类。Factory Method 使一个类的实例化延迟到其子类。
+ * @author xjin
+ * @version 2015/3/4
  */
-class Example
-{
+class Example{
+    
     // The parameterized factory method
-    public static function factory($type)
+    public static function factory($type) 
     {
-        if (include_once 'Drivers/' . $type . '.php') {
+        // 引入不同的文件  创建不同的对象
+        if (include_once 'Drivers/' . $type . '.php') 
+        {
             $classname = 'Driver_' . $type;
-            return new $classname;
+            return new $classname();
         } else {
             throw new Exception('Driver not found');
         }
     }
+
 }
 
 // Load a MySQL Driver
